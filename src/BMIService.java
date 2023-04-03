7d0e4021d986df48b1d3c4844c43fb9f29a8141a
Alex Vdovchenko <demon254@ya.ru>
2023-04-03 21:20:49 +0300

    README.md

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,6 +1,6 @@
 public class BMIService {
-    public double calculate (int weight, double height){
-        double bmi = weight / (height*height); // подсчет
+    public double calculate(int weight, double height) {
+        double bmi = weight / (height * height); // подсчет
         return bmi; // возвращаем рассчитанный индекс массы тела
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         int weight = 89; // вес в килограммах
         double height = 1.87; // рост в метрах
         double bmi = service.calculate(weight, height); // считаем ИМТ
-        String bmiResult = String.format("%.2f",bmi); // округляем до 2-х знаков
+        String bmiResult = String.format("%.2f", bmi); // округляем до 2-х знаков
         System.out.println("Ваш вес: " + weight + " кг.");
-        System.out.println("Ваш рост: " + height*100 + " см.");
+        System.out.println("Ваш рост: " + height * 100 + " см.");
         System.out.println("Ваш индекс массы тела: " + bmiResult);
     }
 }
